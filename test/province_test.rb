d7255162834e49e02a9eef21ad84c4eb4e3e68d9
Chris Miraflor <mirafloc@oregonstate.edu>
2022-05-15 14:14:20 -0700

    Removed redundant fixture setup in province shortfall test

--- test/province_test.rb
+++ test/province_test.rb
@@ -11,8 +11,7 @@ class ProvinceTest < Minitest::Test
   end
 
   def test_province_shortfall
-    asia = Province.new(sample_province_data)
-    assert_equal(5, asia.shortfall)
+    assert_equal(5, @asia.shortfall)
   end
 
   def test_province_profit
